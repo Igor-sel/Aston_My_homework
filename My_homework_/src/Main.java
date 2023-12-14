13de86adc4167d6c04c05b173dc816260955eb99
Igor-sel <115151497+Igor-sel@users.noreply.github.com>
2023-12-14 23:11:11 +0300

    Added task 1

--- My_homework_/src/Main.java
+++ My_homework_/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
 
+    public static void main(String[] args) {
+        printThreeWords();
     }
 }
