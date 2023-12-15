58f15b84c3571b2506c0321de9471a72c812ae4b
Igor-sel <115151497+Igor-sel@users.noreply.github.com>
2023-12-15 17:59:53 +0300

    added task 2

--- My_homework_/src/Main.java
+++ My_homework_/src/Main.java
@@ -1,10 +1,27 @@
 public class Main {
 
+    // Task 1
     public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
     }
 
+    //Task 2
+    public static void checkSumSign() {
+        int a = 1, b = -3;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
     public static void main(String[] args) {
         printThreeWords();
+        System.out.println();
+
+        checkSumSign();
+        System.out.println();
     }
 }
