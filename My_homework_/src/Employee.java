60bd2741b609377b11785732d86347429802dc8c
Igor-sel <115151497+Igor-sel@users.noreply.github.com>
2023-12-28 17:56:26 +0300

    Lesson_4 (Tasks 1-2)

---
+++ My_homework_/src/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    String fullName, position, email;
+    int phoneNum, salary, age;
+
+    public Employee(String fullName, String position, String email, int phoneNum, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.salary = salary;
+        this.age = age;
+    }
+    public void about() {
+        System.out.printf("Ф.И.О.: %s Должность: %s Email: %s Телефон: %s Зарплата: %s Возраст: %d \n", fullName, position, email, phoneNum, salary, age);
+    }
+}
--- My_homework_/src/Main.java
+++ My_homework_/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
+        Employee[] persArray = new Employee[5];
+        persArray[0] = new Employee("Ivanov Ivan", "Engineer", "iv.ivan@mailbox.com", 892312312, 30000, 30);
+        persArray[1] = new Employee("Petrov Peter", "QA", "pet.peter@mailbox.com", 892312001, 25000, 25);
+        persArray[2] = new Employee("Semenov Semen", "BA", "sem.sem@mailbox.com", 892312002, 26000, 26);
+        persArray[3] = new Employee("Sidorov Sergei", "AQA", "sid.serg@mailbox.com", 892312003, 27000, 27);
+        persArray[4] = new Employee("Bykov Alex", "PM", "bykov.alex@mailbox.com", 892312004, 28000, 28);
 
+        for (int i = 0; i < persArray.length; i++) {
+             persArray[i].about();
+        }
     }
 }
